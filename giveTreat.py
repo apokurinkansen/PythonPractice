1fd1b4e11ce2fccd284185e151680c40a10bdcaf
Kubutaku <takuyuu0839@gmail.com>
2022-11-16 18:32:04 +0900

    [fix][2022/11/16]
    giveTreat.py
    奢る人の決定メソッド作成完了

--- giveTreat.py
+++ giveTreat.py
@@ -1,3 +1,5 @@
+import random
+
 class giveTreat:
     name = []
     name_lenghs = 0
@@ -5,6 +7,7 @@ class giveTreat:
 def main():
     show_menu()
     input_name()
+    check_giveHuman()
 
 def show_menu():
     print("-----------------------------")
@@ -16,8 +19,10 @@ def input_name():
     giveTreat.name = input("名前をカンマ(,)区切りで入力してください").split(",")
     giveTreat.name_lenghs = len(giveTreat.name)
 
+def check_giveHuman():
+    num = random.randint(0,giveTreat.name_lenghs)
+    print("奢る人は、",giveTreat.name[num],"で決定!!!!!!")
+
 main()
-        
-setGiveTreat = giveTreat()
 
 
